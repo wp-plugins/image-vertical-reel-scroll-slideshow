3aeec4ac76ed2d2166adb43fece80ae7bd8d5072
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-25 16:23:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/image-vertical-reel-scroll-slideshow/trunk@467475 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,28 @@
+<!--
+##########################################################################################################################
+###### Project   : Image vertical reel scroll slideshow  															######
+###### File Name : image-vertical-reel-scroll-slideshow.js                   										######
+###### Purpose   : This javascript is to scroll the images.  														######
+###### Created   : 24-june-2011                  																	######
+###### Modified  : 24-june-2011                  																	######
+###### Author    : Gopi.R (http://www.gopiplus.com/work/)                       									######
+###### Link      : http://www.gopiplus.org/work/2011/05/30/wordpress-plugin-image-vertical-reel-scroll-slideshow/   ######
+##########################################################################################################################
+-->
+
+
+<div class="wrap">
+  <h2>Plugin Management</h2>
+  Click "Go to - Image Management" button to add/update/delete the images. <br />
+  Click "Go to - Gallery Setting" button to update gallery setting.
+  <h2>Plugin Configuration</h2>
+  <ul>
+  <li>1. Drag and Drop the Widget</li>
+  <li>2. Add directly in the theme</li>
+  <li>3. Short code for pages and posts</li>
+  </ul>
+  <a target="_blank" href='http://www.gopiplus.com/work/2011/05/30/wordpress-plugin-image-vertical-reel-scroll-slideshow/'>Click here for more help and video tutorial</a>
+  <h2>About Plugin</h2>
+  Plug-in created by <a target="_blank" href='http://www.gopiplus.com/work/'>www.gopiplus.com</a><br />
+  Check official website for live demo and video tutorial <a target="_blank" href='http://www.gopiplus.com/work/2011/05/30/wordpress-plugin-image-vertical-reel-scroll-slideshow/'>Click here</a><br />
+</div>
